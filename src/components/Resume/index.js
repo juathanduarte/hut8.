e972c7658ba6e2ce3e9f0fe2d19dfdc88b3f2bfa
Juathan <juathan.cduarte@inf.ufpel.edu.br>
2020-11-17 18:13:09 -0300

    up
    .

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,9 +6,13 @@ function Resume(){
         <div className="tela1">
         <div className="Resume">
             <h1 className="_resume">Resumo</h1>
+            <i className="fas fa-graduation-cap"></i>
             <h2 className="_numeroMatriculado">02</h2>
             <h3 className="_cursosMatriculados">Cursos matriculados</h3>
+            <div className="bottom"></div>
             <div className="block"></div>
+
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha512-0S+nbAYis87iX26mmj/+fWt1MmaKCv80H+Mbo+Ne7ES4I6rxswpfnC6PxmLiw33Ywj2ghbtTw0FkLbMWqh4F7Q==" crossorigin="anonymous" />
         </div>
         </div>
     )
